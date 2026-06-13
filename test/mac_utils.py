d13f9acad23e4e40fc2d5284d02ae378d914898b
Julia Desmazes <julia.desmazes@gmail.com>
2026-06-13 13:59:39 -0500

    fix bad assert

--- test/mac_utils.py
+++ test/mac_utils.py
@@ -130,7 +130,7 @@ async def read_tx_frame(dut) -> bytes:
 	while (dut.phy_tx_v.value == 1):
 		buff.append(dut.phy_tx.value)
 		await ClockCycles(dut.clk, 1)
-	assert len(buff) != (64+8)*4), f"got len {len(buff)}"
+	assert len(buff) == (64+8)*4, f"got len {len(buff)}"
 	return bitpair_to_bytes(buff)
 
 async def check_no_tx_frame(dut, timeout:int = 150) -> None:
